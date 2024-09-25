fdb24bcbb5d38c5ed8909994c78bdd793698941f
Federico.Murru <djfedemurru@icloud.com>
2024-09-25 23:17:43 +0100

    1

--- app/api/case/test/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import prisma from "../../../lib/prisma";
-
-export async function GET() {
-	const patients = await prisma.patient.findMany();
-	return NextResponse.json(patients);
-}
-// ok
